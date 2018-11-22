d4ae4509028bf72b3623c100711142014992a43b
krolpiotr <45247798+krolpiotr@users.noreply.github.com>
2018-11-22 04:13:40 +0100

    Create personnr.py

---
+++ personnr.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print("----------------- The PHOENIX Developer Studio -----------------")
+"""
+@date: 2018-10-30
+@author: Piotr Krol
+@website: simon-phoenix.se
+This program is getting your name, surname and personnummer (ÅÅMMDD-NNNC)
+"""
+
+def num(s):
+    try:
+        return int(s)
+    except ValueError:
+        return str(s)
+
+name = input("Enter your name: ")
+surname = input("Enter your surname: ")
+personnummer = input("Enter your Swedish personnummer: ")
+print("")
+
+gender = personnummer[-1:]
+
+print("Your name is " + name + " " + surname)
+print("Your gender is: %s" % (num(gender) % 2 and 'male' or 'female'))
